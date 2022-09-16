61bfa19ccb4b75e9224b21e898dc583c60c3bc36
root <root@localhost>
2022-09-16 01:55:30 +0000

    Added tests for new features

--- spec/feature/integration_spec.rb
+++ spec/feature/integration_spec.rb
@@ -3,10 +3,13 @@ require 'rails_helper'
 
 RSpec.describe 'Creating a book', type: :feature do
     scenario 'valid inputs' do
-        visit new_book_path
-        fill_in 'Title', with: 'harry potter'
-        click_on 'Create Book'
-        visit books_path
-        expect(page).to have_content('harry potter')
+      visit new_book_path
+      fill_in 'Title', with: 'harry potter'
+      fill_in 'Author', with: 'jk rowling'
+      fill_in 'published_date', with: DateTime.now.to_time
+      fill_in 'Price', with: 1.0
+      click_on 'Create Book'
+      visit books_path
+      expect(page).to have_content('harry potter')
     end
-end
+  end
